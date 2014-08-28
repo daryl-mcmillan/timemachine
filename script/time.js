faebfb94cf27dd4a237e08b2d70d2f084381a8f7
Daryl McMillan <darylm@gmail.com>
2014-08-28 18:50:54 -0400

    simple date parsing

--- script/time.js
+++ script/time.js
@@ -2,6 +2,22 @@ function render_data() {
   var raw = document.getElementById("rawdata").value;
   var pattern = document.getElementById("pattern").value;
   
+  var matcher = new RegExp(pattern,"gim");
+  var matches = raw.match(matcher);
+  if( !matches ) {
+    return;
+  }
+  var matchList = ""
+  for( var i=0; i<matches.length; i++ ) {
+    var m = matches[i];
+    var d = Date.parse(m);
+    if(d) {
+      d = new Date(d);
+      matchList += d.toISOString() + "\n";
+    } else {
+      console.log( {bad:m} );
+    }
+  }
   var parsed = document.getElementById("parsed");
-  parsed.innerText = "asdf\ntest";
+  parsed.innerText = matchList;
 }
