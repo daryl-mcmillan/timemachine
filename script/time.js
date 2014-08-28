a4638d72fda6ea3103c30c8c4a62c4f0a5bb286c
Daryl McMillan <darylm@gmail.com>
2014-08-28 18:29:37 -0400

    basic input

---
+++ script/time.js
@@ -0,0 +1,7 @@
+function render_data() {
+  var raw = document.getElementById("rawdata").value;
+  var pattern = document.getElementById("pattern").value;
+  
+  var parsed = document.getElementById("parsed");
+  parsed.innerText = "asdf\ntest";
+}
